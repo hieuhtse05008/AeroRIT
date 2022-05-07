d2b736d4856ee8803b108e38f21103ee10fb4322
Hà Trung Hiếu <goldenrezgun@gmail.com>
2022-05-07 16:08:10 +0700

    save

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ from torch import optim
 from helpers.augmentations import RandomHorizontallyFlip, RandomVerticallyFlip, \
     RandomTranspose, Compose
 from helpers.utils import AeroCLoader, AverageMeter, Metrics, parse_args
-from helpers.lossfunctions import cross_entropy2d
+from helpers.lossfunctions import GANLoss, cross_entropy2d
 
 from torchvision import transforms
 
@@ -189,8 +189,8 @@ if __name__ == "__main__":
     weights = [1.11, 0.37, 0.56, 4.22, 6.77, 1.0]
     weights = torch.FloatTensor(weights)
     print(torch.cuda.is_available())
-    criterion = cross_entropy2d(reduction='mean', weight=weights.cuda(), ignore_index=5)
-
+    # criterion = cross_entropy2d(reduction='mean', weight=weights.cuda(), ignore_index=5)
+    criterion = GANLoss()
     if args.network_arch == 'resnet':
         net = ResnetGenerator(args.bands, 6, n_blocks=args.resnet_blocks)
     elif args.network_arch == 'segnet':
